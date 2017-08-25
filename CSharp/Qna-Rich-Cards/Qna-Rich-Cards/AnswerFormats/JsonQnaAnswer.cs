61fb453c07c403efcfe4a745ee16c219b98f0c4a
Matt Shim (TEKsystems) <v-shimma@microsoft.com>
2017-08-25 03:50:49 -0700

    added QnA rich cards sample

---
+++ CSharp/Qna-Rich-Cards/Qna-Rich-Cards/AnswerFormats/JsonQnaAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qna_Rich_Cards.AnswerFormats
+{
+    public class JsonQnaAnswer
+    {
+        public string title { get; set; }
+        public string desc { get; set; }
+        public string url { get; set; }
+    }
+}
