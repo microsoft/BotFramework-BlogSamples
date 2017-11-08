7dd474ebba706969edc050c1d2b4a0eeee587fae
Jason Sowers <v-jassow@microsoft.com>
2017-11-08 15:37:53 -0800

    added luis-scorable-qna sample with Matt Shim

---
+++ blog-samples/CSharp/Luis-Scorable-QnA/Luis-Scorable-Qna/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Autofac;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Builder.Internals.Fibers;
+using Microsoft.Bot.Builder.Scorables;
+using Microsoft.Bot.Connector;
+using Scorable.Dialogs;
+
+namespace Scorable
+{
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            this.RegisterBotModules();
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private void RegisterBotModules()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new ReflectionSurrogateModule());
+
+            builder.RegisterModule<GlobalMessageHandlersBotModule>();
+
+            builder.Update(Conversation.Container);
+        }
+    }
+
+    public class GlobalMessageHandlersBotModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder
+                .Register(c => new CommonResponsesScorable(c.Resolve<IDialogTask>()))
+                .As<IScorable<IActivity, double>>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
