43b83bad4c765c7f61ce51080184355120abcdb7
Denise Mak <v-demak@microsoft.com>
2018-06-22 17:34:09 -0700

    initial commit of snippet project for file storage article

---
+++ docs-samples/V4/snippet-projects/bot-builder-howto-v4-storage/CS/how-to-storage/EchoState.cs
@@ -0,0 +1,10 @@
+﻿namespace how_to_storage
+{
+    /// <summary>
+    /// Class for storing conversation state. 
+    /// </summary>
+    public class EchoState
+    {
+        public int TurnCount { get; set; } = 0;
+    }
+}
