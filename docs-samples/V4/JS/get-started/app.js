e8ea5fe3ec5dc8f62e680b4c120b647659351fab
Cash Vo (Aquent LLC) <v-ducvo@microsoft.com>
2018-06-25 11:45:25 -0700

    Getting Started sample - V4 SDK echobot

---
+++ docs-samples/V4/JS/get-started/app.js
@@ -0,0 +1,41 @@
+// Packages are installed for you
+const { BotFrameworkAdapter, MemoryStorage, ConversationState } = require('botbuilder');
+const restify = require('restify');
+
+// Create server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+// Create adapter
+const adapter = new BotFrameworkAdapter({ 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD 
+});
+
+// Add conversation state middleware
+const conversationState = new ConversationState(new MemoryStorage());
+adapter.use(conversationState);
+
+// Listen for incoming requests 
+server.post('/api/messages', (req, res) => {
+    // Route received request to adapter for processing
+    adapter.processActivity(req, res, (context) => {
+        // This bot is only handling Messages
+        if (context.activity.type === 'message') {
+        
+            // Get the conversation state
+            const state = conversationState.get(context);
+            
+            // If state.count is undefined set it to 0, otherwise increment it by 1
+            const count = state.count === undefined ? state.count = 0 : ++state.count;
+            
+            // Echo back to the user whatever they typed.
+            return context.sendActivity(`${count}: You said "${context.activity.text}"`);
+        } else {
+            // Echo back the type of activity the bot detected if not of type message
+            return context.sendActivity(`[${context.activity.type} event detected]`);
+        }
+    });
+});
