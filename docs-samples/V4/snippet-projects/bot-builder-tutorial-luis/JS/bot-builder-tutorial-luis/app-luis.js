58c97fb126855dacd318590d5bf9c410b47265b3
Denise Mak <v-demak@microsoft.com>
2018-06-25 21:25:37 -0700

    stub out intent responses

--- docs-samples/V4/snippet-projects/bot-builder-tutorial-luis/JS/bot-builder-tutorial-luis/app-luis.js
+++ docs-samples/V4/snippet-projects/bot-builder-tutorial-luis/JS/bot-builder-tutorial-luis/app-luis.js
@@ -3,13 +3,10 @@ const { LuisRecognizer } = require('botbuilder-ai');
 const restify = require('restify');
 
 // Replace this appId with the ID of the app you create from cafeLUISModel.json
-const appId = "edaadd9b-b632-4733-a25c-5b67271035dd";
-// const appId = process.env.LUIS_APP_ID; // edaadd9b-b632-4733-a25c-5b67271035dd
+const appId = process.env.LUIS_APP_ID; 
 // Replace this with your authoring key
-const subscriptionKey = "be30825b782843dcbbe520ac5338f567";
-console.log(`LUIS app ID=${appId}, key=${subscriptionKey}`);
-// const subscriptionKey = process.env.LUIS_SUBSCRIPTION_KEY; // be30825b782843dcbbe520ac5338f567
-// console.log(`process.env.LUIS_APP_ID=${process.env.LUIS_APP_ID}, process.env.LUIS_SUBSCRIPTION_KEY=${process.env.LUIS_SUBSCRIPTION_KEY}`);
+const subscriptionKey = process.env.LUIS_SUBSCRIPTION_KEY; 
+console.log(`process.env.LUIS_APP_ID=${process.env.LUIS_APP_ID}, process.env.LUIS_SUBSCRIPTION_KEY=${process.env.LUIS_SUBSCRIPTION_KEY}`);
 // Default is westus
 const serviceEndpoint = 'https://westus.api.cognitive.microsoft.com';
 
@@ -38,48 +35,61 @@ adapter.use(conversationState);
 // Listen for incoming requests 
 server.post('/api/messages', (req, res) => {
     // Route received request to adapter for processing
-    adapter.processActivity(req, res, async (context) => {
+    adapter.processActivity(req, res, (context) => {
         if (context.activity.type === 'message') {
             const state = conversationState.get(context);
-            const count = state.count === undefined ? state.count = 0 : ++state.count;
-            
-            // Call LUIS
-            
-            await luisRec.recognize(context).then(async (res) => 
-                {                  
+
+            // Create dialog context
+            // TODO: IMPORT DIALOGS
+            // const dc = dialogs.createContext(context, state);
+
+            if (!isMessage) {
+                await context.sendActivity(`[${context.activity.type} event detected]`);
+            }
+
+        // Check to see if anyone replied. 
+        if (!context.responded) {
+            await dc.continue();
+            // if the dialog didn't send a response
+            if (!context.responded && isMessage) {
+                await luisRec.recognize(context).then(async (res) => 
+                {                 
                     let topIntent = LuisRecognizer.topIntent(res);    
                     switch (topIntent)
                     {
-                        case "Book_Table": {
+                        case Intents.Book_Table: {
                             await context.sendActivity("Top intent is Book_Table ");                          
-                            //await dc.begin('reserveTable', res);
+                            // await dc.begin('reserveTable', res);
                             break;
                         }
                         
-                        case "Greeting": {
+                        case Intents.Greeting: {
                             await context.sendActivity("Top intent is Greeting");
                             break;
                         }
     
-                        case "Who_are_you_intent": {
+                        case Intents.Who_are_you_intent: {
                             await context.sendActivity("Top intent is Who_are_you_intent");
                             break;
                         }
                         default: {
-                            await context.sendActivity(`Top intent = ${topIntent}`);
+                            await context.sendActivity(`Top intent is ${topIntent}`);
                             // await dc.begin('default', topIntent);
                             break;
                         }
                     }
-    
                 }, (err) => {
-                    // error calling LUIS recognize
+                    // error in recognize()
                     console.log(err);
                 }); 
+            }
+        } 
 
-
+            // Increment count
+            const count = state.count === undefined ? state.count = 0 : ++state.count;
+            return context.sendActivity(`${count}: You said "${context.activity.text}"`);
         } else {
-            await context.sendActivity(`[${context.activity.type} event detected]`);
+            return context.sendActivity(`[${context.activity.type} event detected]`);
         }
     });
 });
