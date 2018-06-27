d9c6a66815516c7a9e40ff66074f356d7abd576d
Cash Vo (Aquent LLC) <v-ducvo@microsoft.com>
2018-06-27 15:18:46 -0700

    [ADD] Conversation flows - with 3 separate scenarios

---
+++ docs-samples/V4/JS/conversation-flows/add-two-numbers.js
@@ -0,0 +1,97 @@
+/*
+ * Botbuilder v4 SDK - Add Two Numbers.
+ * 
+ * This bot demonstrates how to use a single step waterfall. In this case, the dialog simply
+ * add two numbers together whenever the user says something like, "What's 2+3?".
+ * 
+ * To run this bot:
+ * 1) install these npm packages:
+ * npm install --save restify
+ * npm install --save botbuilder@preview
+ * npm install --save botbuilder-dialogs@preview
+ * 
+ * 2) From VSCode, open the package.json file and
+ * Update the property "main" to the name of the sample bot you want to run. 
+ *    For example: "main": "conversation-flows/add-two-numbers.js" to run the this sample bot.
+ * 3) run the bot in debug mode. 
+ * 4) Load the emulator and point it to: http://localhost:3978/api/messages
+ * 5) Send the message "hi" or "reserve table" to engage with the bot.
+ *
+ */ 
+
+// Required packages for this bot
+const { BotFrameworkAdapter, FileStorage, ConversationState, UserState, BotStateSet } = require('botbuilder');
+const restify = require('restify');
+const { DialogSet, TextPrompt, DatetimePrompt, NumberPrompt, ChoicePrompt } = require('botbuilder-dialogs');
+
+// Create server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+// Create adapter
+const adapter = new BotFrameworkAdapter({ 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD 
+});
+
+// Storage
+const storage = new FileStorage("c:/temp"); // Go to this directory to verify the persisted data
+const conversationState = new ConversationState(storage);
+const userState  = new UserState(storage);
+adapter.use(new BotStateSet(conversationState, userState));
+
+const dialogs = new DialogSet();
+
+// Listen for incoming activity 
+server.post('/api/messages', (req, res) => {
+    // Route received activity to adapter for processing
+    adapter.processActivity(req, res, async (context) => {
+        const isMessage = context.activity.type === 'message';
+        // State will store all of your information 
+        const convo = conversationState.get(context);
+        const dc = dialogs.createContext(context, convo);
+
+        if (isMessage) {
+            // MatchesAdd2Numbers checks if the message matches a regular expression
+            // and if it does, returns an array of the numbers to add
+            var numbers = await MatchesAdd2Numbers(context.activity.text); 
+            if (numbers != null && numbers.length >=2 )
+            {    
+                await dc.begin('addTwoNumbers', numbers);
+            }
+        }
+        if (!context.responded) {
+            await dc.continue();
+            // if the dialog didn't send a response
+            if (!context.responded && isMessage) {
+                await dc.context.sendActivity(`Hi! I'm the add 2 numbers bot. Say something like "What's 2+3?"`);
+            }
+        }
+    });
+});
+
+// Add two numbers
+// This sample shows a single step waterfall in a processing an addition equation.
+
+// Show the sum of two numbers.
+dialogs.add('addTwoNumbers', [async function (dc, numbers){
+    var sum = Number.parseFloat(numbers[0]) + Number.parseFloat(numbers[1]);
+    await dc.context.sendActivity(`${numbers[0]} + ${numbers[1]} = ${sum}`);
+    await dc.end();
+}]
+);
+
+async function MatchesAdd2Numbers(message) {
+    const ADD_NUMBERS_REGEXP = /([-+]?(?:[0-9]+(?:\.[0-9]+)?|\.[0-9]+))(?:\s*)\+(?:\s*)([-+]?(?:[0-9]+(?:\.[0-9]+)?|\.[0-9]+))/i;
+    let matched = ADD_NUMBERS_REGEXP.exec(message);
+    if (!matched) {
+        // message wasn't a request to add 2 numbers
+        return null;
+    }
+    else {
+        var numbers = [matched[1], matched[2]];
+        return numbers;
+    }
+}
