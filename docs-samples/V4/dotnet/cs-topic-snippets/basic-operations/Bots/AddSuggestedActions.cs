193907691dadf9967cdc419af52e37822a76246d
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-08-22 15:34:58 -0700

    simplify suggested actions example.

--- docs-samples/V4/dotnet/cs-topic-snippets/basic-operations/Bots/AddSuggestedActions.cs
+++ docs-samples/V4/dotnet/cs-topic-snippets/basic-operations/Bots/AddSuggestedActions.cs
@@ -11,12 +11,7 @@
         {
             // Create the activity and add suggested actions.
             IMessageActivity activity = MessageFactory.SuggestedActions(
-                new CardAction[]
-                {
-                    new CardAction(title: "red", type: ActionTypes.ImBack, value: "red"),
-                    new CardAction( title: "green", type: ActionTypes.ImBack, value: "green"),
-                    new CardAction(title: "blue", type: ActionTypes.ImBack, value: "blue")
-                },
+                actions: new string[] { "red", "green", "blue" },
                 text: "Choose a color");
 
             // Send the activity as a reply to the user.
