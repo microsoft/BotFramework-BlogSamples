70f1e00e9a4d42f034c81468e55d3cf9c86faa09
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-08-22 12:17:24 -0700

    update bot snippets

--- docs-samples/V4/dotnet/cs-topic-snippets/basic-operations/Bots/AddMediaAttachments.cs
+++ docs-samples/V4/dotnet/cs-topic-snippets/basic-operations/Bots/AddMediaAttachments.cs
@@ -13,12 +13,12 @@
             public async Task OnTurnAsync(ITurnContext context, CancellationToken token = default(CancellationToken))
             {
                 // Create the activity and add an attachment.
-                var activity = MessageFactory.Attachment(
+                IMessageActivity activity = MessageFactory.Attachment(
                     new Attachment
                     {
-                        ContentUrl = "https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg",
-                        ContentType = "image/jpeg",
-                        Name = "Bot Framework",
+                        ContentUrl = "imageUrl.png",
+                        ContentType = "image/png",
+                        Name = "imageName",
                     }
                 );
 
@@ -32,23 +32,11 @@
             public async Task OnTurnAsync(ITurnContext context, CancellationToken token = default(CancellationToken))
             {
                 // Create the activity and add an attachment.
-                var activity = MessageFactory.Attachment(new Attachment[]
+                IMessageActivity activity = MessageFactory.Attachment(new Attachment[]
                 {
-                    new Attachment
-                    {
-                        ContentUrl = "https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png",
-                        ContentType = "image/png",
-                    },
-                    new Attachment
-                    {
-                        ContentUrl = "https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png",
-                        ContentType = "image/png",
-                    },
-                    new Attachment
-                    {
-                        ContentUrl = "https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg",
-                        ContentType = "image/jpeg",
-                    },
+                    new Attachment { ContentUrl = "imageUrl1.png", ContentType = "image/png" },
+                    new Attachment { ContentUrl = "imageUrl2.png", ContentType = "image/png" },
+                    new Attachment { ContentUrl = "imageUrl3.png", ContentType = "image/png" },
                 });
 
                 // Send the activity to the user.
@@ -61,26 +49,20 @@
             public async Task OnTurnAsync(ITurnContext context, CancellationToken token = default(CancellationToken))
             {
                 // Create the activity and attach a Hero card.
-                var activity = MessageFactory.Attachment(
-                    new HeroCard(
-                        title: "Get started with the Bot Framework",
-                        images: new CardImage[]
+                IMessageActivity activity = MessageFactory.Attachment(
+                new HeroCard(
+                    title: "heroCardTitle",
+                    images: new CardImage[] { new CardImage { Url = "imageUrl.png" } },
+                    buttons: new CardAction[]
+                    {
+                        new CardAction
                         {
-                            new CardImage
-                            {
-                                Url = "https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg",
-                            },
+                            Type = ActionTypes.OpenUrl,
+                            Title = "Azure Bot Service Documentation",
+                            Value = "https://docs.microsoft.com/azure/bot-service/?view=azure-bot-service-4.0",
                         },
-                        buttons: new CardAction[]
-                        {
-                            new CardAction
-                            {
-                                Type = ActionTypes.OpenUrl,
-                                Title = "Get started",
-                                Value = "https://docs.microsoft.com/en-us/azure/bot-service/",
-                            },
-                        })
-                    .ToAttachment());
+                    })
+                .ToAttachment());
 
                 // Send the activity as a reply to the user.
                 await context.SendActivityAsync(activity, token);
@@ -92,20 +74,22 @@
             public async Task OnTurnAsync(ITurnContext context, CancellationToken token = default(CancellationToken))
             {
                 // Create the activity and attach a Hero card.
-                var activity = MessageFactory.Attachment(
-                    new HeroCard(
-                        title: "Holler Back Buttons",
-                        images: new CardImage[] { new CardImage(url: "imageUrl.png") },
-                        buttons: new CardAction[]
+                IMessageActivity activity = MessageFactory.Attachment(
+                new HeroCard(
+                    title: "Holler Back Buttons",
+                    images: new CardImage[] { new CardImage(url: "imageUrl.png") },
+                    buttons: new CardAction[]
+                    {
+                        new CardAction(title: "Shout Out Loud", type: ActionTypes.ImBack, value: "You can ALL hear me!"),
+                        new CardAction(title: "Much Quieter", type: ActionTypes.PostBack, value: "Shh! My Bot friend hears me."),
+                        new CardAction
                         {
-                            new CardAction(title: "Shout Out Loud", type: ActionTypes.ImBack, value: "You can ALL hear me!"),
-                            new CardAction(title: "Much Quieter", type: ActionTypes.PostBack, value: "Shh! My Bot friend hears me."),
-                            new CardAction(
-                                title: "Show me how to Holler",
-                                type: ActionTypes.OpenUrl,
-                                value: $"https://en.wikipedia.org/wiki/{HeroCard.ContentType}"),
-                        })
-                    .ToAttachment());
+                            Type = ActionTypes.OpenUrl,
+                            Title = "Azure Bot Service Documentation",
+                            Value = "https://docs.microsoft.com/azure/bot-service/?view=azure-bot-service-4.0",
+                        },
+                    })
+                .ToAttachment());
 
                 // Send the activity as a reply to the user.
                 await context.SendActivityAsync(activity, token);
@@ -117,7 +101,7 @@
             public async Task OnTurnAsync(ITurnContext context, CancellationToken token = default(CancellationToken))
             {
                 // Create the activity and attach an Adaptive Card.
-                var card = new AdaptiveCard();
+                AdaptiveCard card = new AdaptiveCard();
                 card.Body.Add(new TextBlock()
                 {
                     Text = "<title>",
@@ -138,7 +122,7 @@
                 card.Actions.Add(new SubmitAction() { Title = "Submit", DataJson = "{ Action:'Submit' }" });
                 card.Actions.Add(new SubmitAction() { Title = "Cancel", DataJson = "{ Action:'Cancel'}" });
 
-                var activity = MessageFactory.Attachment(new Attachment(AdaptiveCard.ContentType, content: card));
+                IMessageActivity activity = MessageFactory.Attachment(new Attachment(AdaptiveCard.ContentType, content: card));
 
                 // Send the activity as a reply to the user.
                 await context.SendActivityAsync(activity, token);
@@ -150,43 +134,34 @@
             public async Task OnTurnAsync(ITurnContext context, CancellationToken token = default(CancellationToken))
             {
                 // Create the activity and attach a set of Hero cards.
-                var activity = MessageFactory.Carousel(
-                    new Attachment[]
-                    {
-                        new HeroCard(
-                            title: "Traffic Manager",
-                            images: new CardImage[]
-                            {
-                                new CardImage(url: "https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png"),
-                            },
-                            buttons: new CardAction[]
-                            {
-                                new CardAction(title: "Traffic Manager", type: ActionTypes.ImBack, value: "traffic manager"),
-                            })
-                        .ToAttachment(),
-                        new HeroCard(
-                            title: "Cloud Service",
-                            images: new CardImage[]
-                            {
-                                new CardImage(url: "https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png"),
-                            },
-                            buttons: new CardAction[]
-                            {
-                                new CardAction(title: "Cloud Service", type: ActionTypes.ImBack, value: "cloud service")
-                            })
-                        .ToAttachment(),
-                        new HeroCard(
-                            title: "Bot Framework",
-                            images: new CardImage[]
-                            {
-                                new CardImage(url: "https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg"),
-                            },
-                            buttons: new CardAction[]
-                            {
-                                new CardAction(title: "Bot Framework", type: ActionTypes.ImBack, value: "bot framework")
-                            })
-                        .ToAttachment()
-                    });
+                IMessageActivity activity = MessageFactory.Carousel(
+                new Attachment[]
+                {
+                    new HeroCard(
+                        title: "cardTitle1",
+                        images: new CardImage[] { new CardImage(url: "imageUrl1.png") },
+                        buttons: new CardAction[]
+                        {
+                            new CardAction(title: "buttonTitle1", type: ActionTypes.ImBack, value: "buttonValue1"),
+                        })
+                    .ToAttachment(),
+                    new HeroCard(
+                        title: "cardTitle2",
+                        images: new CardImage[] { new CardImage(url: "imageUrl2.png") },
+                        buttons: new CardAction[]
+                        {
+                            new CardAction(title: "buttonTitle2", type: ActionTypes.ImBack, value: "buttonValue2")
+                        })
+                    .ToAttachment(),
+                    new HeroCard(
+                        title: "cardTitle3",
+                        images: new CardImage[] { new CardImage(url: "imageUrl3.png") },
+                        buttons: new CardAction[]
+                        {
+                            new CardAction(title: "buttonTitle3", type: ActionTypes.ImBack, value: "buttonValue3")
+                        })
+                    .ToAttachment()
+                });
 
                 // Send the activity as a reply to the user.
                 await context.SendActivityAsync(activity, token);
