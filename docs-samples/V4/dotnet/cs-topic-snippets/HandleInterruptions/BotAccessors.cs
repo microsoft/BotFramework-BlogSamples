69003884d52aab09e3704c2c323f232059509611
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-09-19 15:15:37 -0700

    Add a sample for the interrupts topic.

---
+++ docs-samples/V4/dotnet/cs-topic-snippets/HandleInterruptions/BotAccessors.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace ComplexConversation
+{
+    using System;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Builder.Dialogs;
+
+    /// <summary>
+    /// The bot's state and state property accessor objects.
+    /// </summary>
+    public class BotAccessors
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BotAccessors"/> class.
+        /// Contains the <see cref="ConversationState"/> and associated <see cref="IStatePropertyAccessor{T}"/>.
+        /// </summary>
+        /// <param name="conversationState">The state object that stores the counter.</param>
+        public BotAccessors(ConversationState conversationState)
+        {
+            ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="IStatePropertyAccessor{T}"/> for CounterState.
+        /// </summary>
+        /// <value>
+        /// The accessor stores the turn count for the conversation.
+        /// </value>
+        public IStatePropertyAccessor<DialogState> DialogStateAccessor { get; set; }
+
+        /// <summary>
+        /// Gets the <see cref="ConversationState"/> object for the conversation.
+        /// </summary>
+        /// <value>The <see cref="ConversationState"/> object.</value>
+        public ConversationState ConversationState { get; }
+    }
+}
---
+++ docs-samples/V4/dotnet/cs-topic-snippets/HandleInterruptions/ComplexConversationBot.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace ComplexConversation
+{
+    /// <summary>
+    /// Represents a bot that processes incoming activities.
+    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
+    /// This is a Transient lifetime service.  Transient lifetime services are created
+    /// each time they're requested. For each Activity received, a new instance of this
+    /// class is created. Objects that are expensive to construct, or have a lifetime
+    /// beyond the single turn, should be carefully managed.
+    /// For example, the <see cref="MemoryStorage"/> object and associated
+    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
+    public class ComplexConversationBot : IBot
+    {
+        private readonly BotAccessors _accessors;
+        private readonly OrderDinnerDialogs _dialogs;
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComplexConversationBot"/> class.
+        /// </summary>
+        /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> used to manage state.</param>
+        /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
+        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
+        public ComplexConversationBot(BotAccessors accessors, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<ComplexConversationBot>();
+            _logger.LogTrace("EchoBot turn start.");
+            _accessors = accessors ?? throw new System.ArgumentNullException(nameof(accessors));
+            _dialogs = new OrderDinnerDialogs(_accessors.DialogStateAccessor);
+        }
+
+        /// <summary>
+        /// Every conversation turn for our Echo Bot will call this method.
+        /// There are no dialogs used, since it's "single turn" processing, meaning a single
+        /// request and response.
+        /// </summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        /// <seealso cref="BotStateSet"/>
+        /// <seealso cref="ConversationState"/>
+        /// <seealso cref="IMiddleware"/>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var dc = await _dialogs.CreateContextAsync(turnContext, cancellationToken);
+
+            switch (turnContext.Activity.Type)
+            {
+                case ActivityTypes.Message:
+
+                    await dc.ContinueDialogAsync(cancellationToken);
+                    if (!turnContext.Responded)
+                    {
+                        await dc.BeginDialogAsync(OrderDinnerDialogs.MainDialogId, null, cancellationToken);
+                    }
+
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+
+                    var activity = turnContext.Activity.AsConversationUpdateActivity();
+                    if (activity.MembersAdded.Any(member => member.Id != activity.Recipient.Id))
+                    {
+                        await dc.BeginDialogAsync(OrderDinnerDialogs.MainDialogId, null, cancellationToken);
+                    }
+
+                    break;
+            }
+
+            await _accessors.ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+    }
+}
