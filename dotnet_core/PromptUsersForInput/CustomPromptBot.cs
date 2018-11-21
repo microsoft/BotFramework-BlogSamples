665e98cec4919cc106d7ab1664602cd9d28824dd
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-11-21 11:38:45 -0800

    Add backing projects for the how to state and primitive prompt topics.

---
+++ dotnet_core/PromptUsersForInput/CustomPromptBot.cs
@@ -0,0 +1,263 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.DateTime;
+using Microsoft.Recognizers.Text.Number;
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// Represents a bot that processes incoming activities.
+    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
+    /// This is a Transient lifetime service.  Transient lifetime services are created
+    /// each time they're requested. For each Activity received, a new instance of this
+    /// class is created. Objects that are expensive to construct, or have a lifetime
+    /// beyond the single turn, should be carefully managed.
+    /// For example, the <see cref="MemoryStorage"/> object and associated
+    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
+    public class CustomPromptBot : IBot
+    {
+        private readonly CustomPromptBotAccessors _accessors;
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomPromptBot"/> class.
+        /// </summary>
+        /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> used to manage state.</param>
+        /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
+        /// <remarks>Defines a bot for filling a user profile.</remarks>
+        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
+        public CustomPromptBot(CustomPromptBotAccessors accessors, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<CustomPromptBot>();
+            _logger.LogTrace("EchoBot turn start.");
+            _accessors = accessors ?? throw new System.ArgumentNullException(nameof(accessors));
+        }
+
+        /// <summary>The turn handler for the bot.</summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Handle Message activity type, which is the main activity type for shown within a conversational interface
+            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
+            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                // Get the state properties from the turn context.
+                ConversationFlow flow = await _accessors.ConversationFlowAccessor.GetAsync(turnContext, () => new ConversationFlow());
+                UserProfile profile = await _accessors.UserProfileAccessor.GetAsync(turnContext, () => new UserProfile());
+
+                await FillOutUserProfileAsync(flow, profile, turnContext);
+
+                // Update state and save changes.
+                await _accessors.ConversationFlowAccessor.SetAsync(turnContext, flow);
+                await _accessors.ConversationState.SaveChangesAsync(turnContext);
+
+                await _accessors.UserProfileAccessor.SetAsync(turnContext, profile);
+                await _accessors.UserState.SaveChangesAsync(turnContext);
+            }
+        }
+
+        /// <summary>
+        /// Manages the conversation flow for filling out the user's profile, including parsing and validation.
+        /// </summary>
+        /// <param name="flow">The conversation flow state property.</param>
+        /// <param name="profile">The user profile state property.</param>
+        /// <param name="turnContext">The context object for the current turn.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        private static async Task FillOutUserProfileAsync(ConversationFlow flow, UserProfile profile, ITurnContext turnContext)
+        {
+            string input = turnContext.Activity.Text?.Trim();
+            string message;
+            switch (flow.LastQuestionAsked)
+            {
+                case ConversationFlow.Question.None:
+                    await turnContext.SendActivityAsync("Let's get started. What is your name?");
+                    flow.LastQuestionAsked = ConversationFlow.Question.Name;
+                    break;
+                case ConversationFlow.Question.Name:
+                    if (ValidateName(input, out string name, out message))
+                    {
+                        profile.Name = name;
+                        await turnContext.SendActivityAsync($"Hi {profile.Name}.");
+                        await turnContext.SendActivityAsync("How old are you?");
+                        flow.LastQuestionAsked = ConversationFlow.Question.Age;
+                        break;
+                    }
+                    else
+                    {
+                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.");
+                        break;
+                    }
+
+                case ConversationFlow.Question.Age:
+                    if (ValidateAge(input, out int age, out message))
+                    {
+                        profile.Age = age;
+                        await turnContext.SendActivityAsync($"I have your age as {profile.Age}.");
+                        await turnContext.SendActivityAsync("When is your flight?");
+                        flow.LastQuestionAsked = ConversationFlow.Question.Date;
+                        break;
+                    }
+                    else
+                    {
+                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.");
+                        break;
+                    }
+
+                case ConversationFlow.Question.Date:
+                    if (ValidateDate(input, out string date, out message))
+                    {
+                        profile.Date = date;
+                        await turnContext.SendActivityAsync($"Your cab ride to the airport is scheduled for {profile.Date}.");
+                        await turnContext.SendActivityAsync($"Thanks for completing the booking {profile.Name}.");
+                        await turnContext.SendActivityAsync($"Type anything to run the bot again.");
+                        flow.LastQuestionAsked = ConversationFlow.Question.None;
+                        profile = new UserProfile();
+                        break;
+                    }
+                    else
+                    {
+                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.");
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Validates name input.
+        /// </summary>
+        /// <param name="input">The user's input.</param>
+        /// <param name="name">When the method returns, contains the normalized name, if validation succeeded.</param>
+        /// <param name="message">When the method returns, contains a message with which to reprompt, if validation failed.</param>
+        /// <returns>indicates whether validation succeeded.</returns>
+        private static bool ValidateName(string input, out string name, out string message)
+        {
+            name = null;
+            message = null;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                message = "Please enter a name that contains at least one character.";
+            }
+            else
+            {
+                name = input.Trim();
+            }
+
+            return message is null;
+        }
+
+        /// <summary>
+        /// Validates age input.
+        /// </summary>
+        /// <param name="input">The user's input.</param>
+        /// <param name="age">When the method returns, contains the normalized age, if validation succeeded.</param>
+        /// <param name="message">When the method returns, contains a message with which to reprompt, if validation failed.</param>
+        /// <returns>indicates whether validation succeeded.</returns>
+        private static bool ValidateAge(string input, out int age, out string message)
+        {
+            age = 0;
+            message = null;
+
+            // Try to recognize the input as a number. This works for responses such as "twelve" as well as "12".
+            try
+            {
+                // Attempt to convert the Recognizer result to an integer. This works for "a dozen", "twelve", "12", and so on.
+                // The recognizer returns a list of potential recognition results, if any.
+                List<ModelResult> results = NumberRecognizer.RecognizeNumber(input, Culture.English);
+                foreach (ModelResult result in results)
+                {
+                    // The result resolution is a dictionary, where the "value" entry contains the processed string.
+                    if (result.Resolution.TryGetValue("value", out object value))
+                    {
+                        age = Convert.ToInt32(value);
+                        if (age >= 18 && age <= 120)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                message = "Please enter an age between 18 and 120.";
+            }
+            catch
+            {
+                message = "I'm sorry, I could not interpret that as an age. Please enter an age between 18 and 120.";
+            }
+
+            return message is null;
+        }
+
+        /// <summary>
+        /// Validates flight time input.
+        /// </summary>
+        /// <param name="input">The user's input.</param>
+        /// <param name="date">When the method returns, contains the normalized date, if validation succeeded.</param>
+        /// <param name="message">When the method returns, contains a message with which to reprompt, if validation failed.</param>
+        /// <returns>indicates whether validation succeeded.</returns>
+        private static bool ValidateDate(string input, out string date, out string message)
+        {
+            date = null;
+            message = null;
+
+            // Try to recognize the input as a date-time. This works for responses such as "11/14/2018", "9pm", "tomorrow", "Sunday at 5pm", and so on.
+            // The recognizer returns a list of potential recognition results, if any.
+            try
+            {
+                List<ModelResult> results = DateTimeRecognizer.RecognizeDateTime(input, Culture.English);
+
+                // Check whether any of the recognized date-times are appropriate,
+                // and if so, return the first appropriate date-time. We're checking for a value at least an hour in the future.
+                DateTime earliest = DateTime.Now.AddHours(1.0);
+                foreach (ModelResult result in results)
+                {
+                    // The result resolution is a dictionary, where the "values" entry contains the processed input.
+                    List<Dictionary<string, string>> resolutions = result.Resolution["values"] as List<Dictionary<string, string>>;
+                    foreach (Dictionary<string, string> resolution in resolutions)
+                    {
+                        // The processed input contains a "value" entry if it is a date-time value, or "start" and
+                        // "end" entries if it is a date-time range.
+                        if (resolution.TryGetValue("value", out string dateString)
+                            || resolution.TryGetValue("start", out dateString))
+                        {
+                            if (DateTime.TryParse(dateString, out DateTime candidate)
+                                && earliest < candidate)
+                            {
+                                date = candidate.ToShortDateString();
+                                return true;
+                            }
+                        }
+                    }
+                }
+
+                message = "I'm sorry, please enter a date at least an hour out.";
+            }
+            catch
+            {
+                message = "I'm sorry, I could not interpret that as an appropriate date. Please enter a date at least an hour out.";
+            }
+
+            return false;
+        }
+    }
+}
