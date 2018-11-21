9bed14504432f97ef586a99bff66134ebd031d75
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-11-21 11:50:17 -0800

    Add backing projects for the how to state and primitive prompt topics. (#45)

---
+++ dotnet_core/PromptUsersForInput/CustomPromptBotAccessors.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Bot.Builder;
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// This class is created as a Singleton and passed into the IBot-derived constructor.
+    ///  - See <see cref="CustomPromptBotAccessors"/> constructor for how that is injected.
+    ///  - See the Startup.cs file for more details on creating the Singleton that gets
+    ///    injected into the constructor.
+    /// </summary>
+    public class CustomPromptBotAccessors
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomPromptBotAccessors"/> class.
+        /// Contains the state management and associated accessor objects.
+        /// </summary>
+        /// <param name="conversationState">The state object that stores the conversation state.</param>
+        /// <param name="userState">The state object that stores the user state.</param>
+        public CustomPromptBotAccessors(ConversationState conversationState, UserState userState)
+        {
+            ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+            UserState = userState ?? throw new ArgumentNullException(nameof(userState));
+        }
+
+        /// <summary>
+        /// Gets the accessor name for the conversation flow property.
+        /// </summary>
+        /// <value>The accessor name for the conversation flow property.</value>
+        /// <remarks>Accessors require a unique name.</remarks>
+        public static string ConversationFlowName { get; } = "ConversationFlow";
+
+        /// <summary>
+        /// Gets the accessor name for the user profile property accessor.
+        /// </summary>
+        /// <value>The accessor name for the user profile property accessor.</value>
+        /// <remarks>Accessors require a unique name.</remarks>
+        public static string UserProfileName { get; } = "UserProfile";
+
+        /// <summary>
+        /// Gets or sets the <see cref="IStatePropertyAccessor{T}"/> for the conversation flow property.
+        /// </summary>
+        /// <value>
+        /// The accessor stores the turn count for the conversation.
+        /// </value>
+        public IStatePropertyAccessor<ConversationFlow> ConversationFlowAccessor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="IStatePropertyAccessor{T}"/> for the user profile property.
+        /// </summary>
+        /// <value>
+        /// The accessor stores the turn count for the conversation.
+        /// </value>
+        public IStatePropertyAccessor<UserProfile> UserProfileAccessor { get; set; }
+
+        /// <summary>
+        /// Gets the <see cref="ConversationState"/> object for the conversation.
+        /// </summary>
+        /// <value>The <see cref="ConversationState"/> object.</value>
+        public ConversationState ConversationState { get; }
+
+        /// <summary>
+        /// Gets the <see cref="UserState"/> object for the conversation.
+        /// </summary>
+        /// <value>The <see cref="UserState"/> object.</value>
+        public UserState UserState { get; }
+    }
+}
