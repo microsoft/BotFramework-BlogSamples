30e2c099c7e65627aa2792c887895944a4794323
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-11-27 12:25:49 -0800

    Create C# sample project

---
+++ SDKV4-Samples/dotnet_core/ComplexDialogBot/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotBuilderSamples
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// This is our application state. Just a regular serializable .NET class.
+    /// </summary>
+    public class UserProfile
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<string> CompaniesToReview { get; set; } = new List<string>();
+    }
+}
