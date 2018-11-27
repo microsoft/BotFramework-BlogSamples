1cb397ba4201f41102ae77c80956ef6e10bc76ac
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-11-27 14:41:17 -0800

    Update code and code comments.

--- SDKV4-Samples/dotnet_core/ComplexDialogBot/UserProfile.cs
+++ SDKV4-Samples/dotnet_core/ComplexDialogBot/UserProfile.cs
@@ -5,15 +5,19 @@ namespace Microsoft.BotBuilderSamples
 {
     using System.Collections.Generic;
 
-    /// <summary>
-    /// This is our application state. Just a regular serializable .NET class.
-    /// </summary>
+    /// <summary>Contains information about a user.</summary>
     public class UserProfile
     {
+        /// <summary>Gets or sets the user's name.</summary>
+        /// <value>The user's name.</value>
         public string Name { get; set; }
 
+        /// <summary>Gets or sets the user's age.</summary>
+        /// <value>The user's age.</value>
         public int Age { get; set; }
 
+        /// <summary>Gets or sets the list of companies the user wants to review.</summary>
+        /// <value>The list of companies the user wants to review.</value>
         public List<string> CompaniesToReview { get; set; } = new List<string>();
     }
 }
