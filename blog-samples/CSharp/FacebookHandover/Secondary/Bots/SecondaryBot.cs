96fb09cf57f842b176956e0fe16ff98b78828fd2
Kyle Delaney <v-kydela@microsoft.com>
2019-05-24 09:56:30 -0700

    Make secondary bot

--- blog-samples/CSharp/FacebookHandover/Secondary/Bots/SecondaryBot.cs
+++ blog-samples/CSharp/FacebookHandover/Secondary/Bots/SecondaryBot.cs
@@ -3,41 +3,38 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using FacebookModel;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
-using FacebookModel;
-using System;
 using Newtonsoft.Json;
-using Microsoft.Bot.Connector;
+using Newtonsoft.Json.Linq;
 
-namespace Primary.Bots
+namespace Secondary.Bots
 {
-	/// <summary>
-	/// This is based on Eric Dahlvang's FacebookEventsBotExpanded: https://github.com/EricDahlvang/FacebookEventsBotExpanded
-	/// </summary>
-	public class PrimaryBot : ActivityHandler
+	public class SecondaryBot : ActivityHandler
 	{
 		private const string OPTION_PASS_PAGE_INBOX = "Pass to page inbox";
-		private const string OPTION_PASS_SECONDARY_BOT = "Pass to secondary";
+		private const string OPTION_PASS_PRIMARY_BOT = "Pass to primary";
 		private const string OPTION_REQUEST_THREAD_CONTROL = "Receive request";
 		private const string OPTION_REQUEST_THREAD_CONTROL_NICELY = "Receive nice request";
 		private const string OPTION_TAKE_THREAD_CONTROL = "Have control taken";
 		private const string PAGE_INBOX_ID = "263902037430900";
 
-		private static readonly string[] _options = new[] { OPTION_PASS_PAGE_INBOX, OPTION_PASS_SECONDARY_BOT, OPTION_REQUEST_THREAD_CONTROL, OPTION_REQUEST_THREAD_CONTROL_NICELY };
+		private static readonly string[] _options = new[] { OPTION_PASS_PAGE_INBOX, OPTION_PASS_PRIMARY_BOT, OPTION_TAKE_THREAD_CONTROL };
 
 		private readonly ILogger _logger;
 		private readonly IConfiguration _configuration;
 
-		public PrimaryBot(ILogger<PrimaryBot> logger, IConfiguration configuration)
+		public SecondaryBot(ILogger<SecondaryBot> logger, IConfiguration configuration)
 		{
 			_logger = logger;
 			_configuration = configuration;
@@ -54,25 +51,14 @@ namespace Primary.Bots
 					await FacebookThreadControlHelper.PassThreadControlAsync(_configuration["FacebookPageToken"], PAGE_INBOX_ID, turnContext.Activity.From.Id, text);
 					break;
 
-				case OPTION_PASS_SECONDARY_BOT:
-
-					var secondaryReceivers = await FacebookThreadControlHelper.GetSecondaryReceiversAsync(_configuration["FacebookPageToken"]);
-
-					foreach (var receiver in secondaryReceivers)
-					{
-						if (receiver != PAGE_INBOX_ID)
-						{
-							await turnContext.SendActivityAsync("Passing thread control to the secondary app.");
-							await FacebookThreadControlHelper.PassThreadControlAsync(_configuration["FacebookPageToken"], receiver, turnContext.Activity.From.Id, text);
-							break;
-						}
-					}
+				case OPTION_PASS_PRIMARY_BOT:
+					await turnContext.SendActivityAsync("Passing thread control to the primary app.");
+					await FacebookThreadControlHelper.PassThreadControlAsync(_configuration["FacebookPageToken"], _configuration["FacebookPrimaryAppID"], turnContext.Activity.From.Id, text);
 
 					break;
 
-				case OPTION_REQUEST_THREAD_CONTROL:
-				case OPTION_REQUEST_THREAD_CONTROL_NICELY:
-					// Do nothing because the secondary receiver should react to these instead
+				case OPTION_TAKE_THREAD_CONTROL:
+					// Do nothing because the primary receiver should react to this instead
 					break;
 
 				default:
@@ -83,7 +69,7 @@ namespace Primary.Bots
 
 		protected override async Task OnConversationUpdateActivityAsync(ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
 		{
-			_logger.LogInformation("PrimaryBot - Processing a ConversationUpdate Activity.");
+			_logger.LogInformation("SecondaryBot - Processing a ConversationUpdate Activity.");
 
 			var facebookPayload = (turnContext.Activity.ChannelData as JObject)?.ToObject<FacebookPayload>();
 
@@ -105,7 +91,7 @@ namespace Primary.Bots
 		}
 		protected override async Task OnEventActivityAsync(ITurnContext<IEventActivity> turnContext, CancellationToken cancellationToken)
 		{
-			_logger.LogInformation("PrimaryBot - Processing an Event Activity.");
+			_logger.LogInformation("SecondaryBot - Processing an Event Activity.");
 
 			// Analyze Facebook payload from EventActivity.Value
 			await ProcessFacebookMessage(turnContext, turnContext.Activity.Value, cancellationToken);
@@ -113,8 +99,7 @@ namespace Primary.Bots
 
 		private async Task<bool> ProcessFacebookMessage(ITurnContext turnContext, object data, CancellationToken cancellationToken)
 		{
-			return await ProcessStandbyPayload(turnContext, data, cancellationToken)
-				|| await ProcessFacebookPayload(turnContext, data, cancellationToken);
+			return await ProcessStandbyPayload(turnContext, data, cancellationToken);
 		}
 
 		private async Task<bool> ProcessStandbyPayload(ITurnContext turnContext, object data, CancellationToken cancellationToken)
@@ -137,62 +122,17 @@ namespace Primary.Bots
 
 		protected virtual async Task OnFacebookStandby(ITurnContext turnContext, FacebookStandby facebookStandby, CancellationToken cancellationToken)
 		{
-			_logger.LogInformation("PrimaryBot - Standby message received.");
+			_logger.LogInformation("SecondaryBot - Standby message received.");
 
 			var text = facebookStandby?.Message?.Text;
 
-			if (text?.Equals(OPTION_TAKE_THREAD_CONTROL, StringComparison.InvariantCultureIgnoreCase) == true)
+			if (text?.Equals(OPTION_REQUEST_THREAD_CONTROL, StringComparison.InvariantCultureIgnoreCase) == true)
 			{
-				await FacebookThreadControlHelper.TakeThreadControlAsync(_configuration["FacebookPageToken"], facebookStandby.Sender.Id, text);
+				await FacebookThreadControlHelper.RequestThreadControlAsync(_configuration["FacebookPageToken"], facebookStandby.Sender.Id, "give me control");
 			}
-		}
-
-		private async Task<bool> ProcessFacebookPayload(ITurnContext turnContext, object data, CancellationToken cancellationToken)
-		{
-			try
+			else if (text?.Equals(OPTION_REQUEST_THREAD_CONTROL_NICELY, StringComparison.InvariantCultureIgnoreCase) == true)
 			{
-				var facebookPayload = (data as JObject)?.ToObject<FacebookPayload>();
-				if (facebookPayload != null)
-				{
-					// At this point we know we are on Facebook channel, and can consume the Facebook custom payload
-					// present in channelData.
-
-					// Thread Control Request
-					if (facebookPayload.RequestThreadControl != null)
-					{
-						await OnFacebookThreadControlRequest(turnContext, facebookPayload, cancellationToken);
-						return true;
-					}
-				}
-			}
-			catch (JsonSerializationException)
-			{
-				if (turnContext.Activity.ChannelId != Channels.Facebook)
-				{
-					await turnContext.SendActivityAsync("This sample is intended to be used with a Facebook bot.");
-				}
-			}
-
-			return false;
-		}
-
-		protected virtual async Task OnFacebookThreadControlRequest(ITurnContext turnContext, FacebookPayload facebookPayload, CancellationToken cancellationToken)
-		{
-			_logger.LogInformation("PrimaryBot - Thread Control Request message received.");
-
-			if (facebookPayload.RequestThreadControl.Metadata == "please")
-			{
-				await turnContext.SendActivityAsync("The secondary app requested thread control. Passing thread control to the secondary app...");
-
-				await FacebookThreadControlHelper.PassThreadControlAsync(
-					_configuration["FacebookPageToken"],
-					facebookPayload.RequestThreadControl.RequestedOwnerAppId,
-					facebookPayload.Sender.Id,
-					"allowing thread control");
-			}
-			else
-			{
-				await turnContext.SendActivityAsync("The secondary app requested thread control but did not ask nicely. Thread control will not be passed");
+				await FacebookThreadControlHelper.RequestThreadControlAsync(_configuration["FacebookPageToken"], facebookStandby.Sender.Id, "please");
 			}
 		}
 
--- blog-samples/CSharp/FacebookHandover/Secondary/Startup.cs
+++ blog-samples/CSharp/FacebookHandover/Secondary/Startup.cs
@@ -37,7 +37,7 @@ namespace Secondary
 			services.AddSingleton<IBotFrameworkHttpAdapter, BotFrameworkHttpAdapter>();
 
 			// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-			services.AddTransient<IBot, EchoBot>();
+			services.AddTransient<IBot, SecondaryBot>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
